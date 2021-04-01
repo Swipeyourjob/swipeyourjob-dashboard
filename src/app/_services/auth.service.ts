b17f400363e97ccad93fc05681b71ebff1cc8dd7
Dahir Warsame <dahirwarsame@gmail.com>
2021-04-01 16:38:34 +0200

    change account.service to auth.service & added auth.intercepter as a pre to removing the fake backend

---
+++ src/app/_services/auth.service.ts
@@ -0,0 +1,62 @@
+﻿import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { environment } from '@environments/environment';
+import { User } from '@app/_models';
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+    private userSubject: BehaviorSubject<User>;
+    public user: Observable<User>;
+
+    constructor(
+        private router: Router,
+        private http: HttpClient
+    ) {
+    }
+
+    public get userValue(): User {
+        return this.userSubject.value;
+    }
+
+    login(username: string, password: string) {
+        return this.http.post<User>(`${environment.apiUrl}/users/authenticate`, { username, password })
+            .pipe(map(user => {
+                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                localStorage.setItem('user', JSON.stringify(user));
+                this.userSubject.next(user);
+                return user;
+            }));
+    }
+
+    login(username: string, password: string): Observable<any> {
+        return this.http.post(environment.apiUrl + 'login', {
+          username,
+          password
+        }, httpOptions);
+      }
+    
+    logout() {
+        // remove user from local storage and set current user to null
+        localStorage.removeItem('user');
+        this.userSubject.next(null);
+        this.router.navigate(['/account/login']);
+    }
+    // TODO:: ask zyad for CRUD api capability GET/POST/PUT/DELETE for users
+    register(username: string, email: string, password: string): Observable<any> {
+        return this.http.post(environment.apiUri + 'login', {
+          username,
+          email,
+          password
+        }, httpOptions);
+      }
+
+
+}
