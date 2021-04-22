1f595b2bcdefcaf6803ba15d1c375451adc502a3
Zyad Osseyran <zyad.osseyran@ingrammicro.com>
2021-04-22 21:56:33 +0200

    email validation added

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+
+
+import {TokenStorageService } from '../_services';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  form: any = {
+    email: "",
+    password: null
+  };
+  isLoggedIn = false;
+  constructor(private tokenStorage: TokenStorageService) { }
 
   ngOnInit(): void {
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+    }
+  }
+  onSubmit(): void{
+
   }
 
 }
