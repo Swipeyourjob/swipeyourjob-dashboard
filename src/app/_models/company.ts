4235c5880ad29a12beee84924c97e29cb3d854e2
Dahir Warsame <dahirwarsame@gmail.com>
2021-05-04 11:19:38 +0200

    removed old model class

--- src/app/_models/company.ts
+++ src/app/_models/company.ts
@@ -1,22 +1,3 @@
-export class Company {
-    constructor(
-        public companyinfo: CompanyInfo,
-        public companylocation: CompanyLocation,
-        public companyimages: string
-    ) { }
-
-}
-export class CompanyInfo {
-    id!: number;
-    title!: string;
-    description!: string;
-}
-export class CompanyLocation {
-    city!: string;
-    streetname!: string;
-    housenumber!: string;
-    zipcode!: string;
-}
 export interface CompanyI {
     companyname: string;
     zipcode: string;
