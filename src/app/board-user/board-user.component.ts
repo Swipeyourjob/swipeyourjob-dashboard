02ed6617f73640c05444a721184ac29b6a502d76
BartHiemstra <bart.hiemstra@student.hu.nl>
2021-05-27 11:48:00 +0200

    Minor fixes

--- src/app/board-user/board-user.component.ts
+++ src/app/board-user/board-user.component.ts
@@ -12,13 +12,6 @@ export class BoardUserComponent implements OnInit {
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
-    this.userService.getUserBoard().subscribe(
-      data => {
-        this.content = data;
-      },
-      err => {
-        this.content = JSON.parse(err.error).message;
-      }
-    );
+    
   }
 }
