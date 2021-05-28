6a0e7ee318f45eb3020c58c684026aedd4a6d8b1
BartHiemstra <bart.hiemstra@student.hu.nl>
2021-05-28 13:33:18 +0200

    Fixed phrasing of some text, cleaned some code snippets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,25 +24,21 @@ export class AppComponent {
         this.tokenStorageService.useRememberMe();
         this.isLoggedIn = !!this.tokenStorageService.getToken();
 
-        if (this.isLoggedIn) {
+        if(this.isLoggedIn) {
             const user = this.tokenStorageService.getUser();
             this.roles = user.roles;
+
+            console.log(user.roles);
 
             this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
             this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
 
             this.username = user.username;
             this.sidebar.nativeElement.style.display = 'block';
-        }else{
+        } 
+        else {
             this.sidebar.nativeElement.style.display = 'none';
         }
-
-        /*
-        $('#sidebarCollapse').on('click', function () {
-            console.log('KLIK');
-            $('#sidebar').toggleClass('active');
-        });
-        */
     }
 
     toggleMenu(): void {
