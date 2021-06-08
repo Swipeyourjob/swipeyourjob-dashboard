2d3dce9da0931254c2e5644944ae14948f60782b
Dahir Warsame <dahirwarsame@gmail.com>
2021-06-08 15:40:32 +0200

    added upload api function

--- src/app/_services/establishment.service.ts
+++ src/app/_services/establishment.service.ts
@@ -10,12 +10,15 @@ import { catchError, retry } from 'rxjs/operators';
 const httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
 };
+const httpUploadOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'multipart/form-data' }),
+};
 
 @Injectable({
     providedIn: 'root',
 })
 export class EstablishmentService {
-    
+
     constructor(
         private router: Router,
         private http: HttpClient,
@@ -24,9 +27,13 @@ export class EstablishmentService {
 
     }
     public getUserEstamblishments() {
-        return this.http.get<Establishment>(`${environment.apiUrl}/getEstablishmentProfile`,httpOptions);
+        return this.http.get<Establishment>(`${environment.apiUrl}/getEstablishmentProfile`, httpOptions);
     }
-    public getEstamblishmentByID(id: number){
-        return this.http.get(`${environment.apiUrl}/getEstablishmentProfile/${id}`,httpOptions);
+    public getEstamblishmentByID(id: number) {
+        return this.http.get(`${environment.apiUrl}/getEstablishmentProfile/${id}`, httpOptions);
+    }
+    public uploadCompanyImg(imageFile: any) {
+
+        return this.http.post(`${environment.apiUrl}/uploadimage`, {imageFile}, httpUploadOptions);
     }
 }
