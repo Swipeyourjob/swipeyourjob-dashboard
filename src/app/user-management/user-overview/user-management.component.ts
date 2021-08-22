2e59ca4cc79ed4deeed845d0a594a85eab831fc4
Zyad Osseyran <zyadosseyran@gmail.com>
2021-08-22 13:01:33 +0200

    first poc of chat finished

---
+++ src/app/user-management/user-overview/user-management.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usermanagement',
+  templateUrl: './user-overview.component.html',
+  styleUrls: ['./user-overview.component.css']
+})
+export class UsermanagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
